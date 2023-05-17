bebccbdab0627958a351f3e196fceeb4a3858635
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2023-05-17 17:16:20 -0600

    test
    test

--- ReactiveWebsite/myapp/src/routes/+layout.svelte
+++ ReactiveWebsite/myapp/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <nav class="navbar">
     <a href="/">home</a>
     <a href="/about">about</a>
+    <a href="/tutorial"></a>
 </nav>
 
 <style>
@@ -8,13 +9,15 @@
         border: #ECD444;
         border-bottom-style: solid;
         border-bottom-width: 1px;
-        padding-bottom: 20px;
+        margin-top: 15px;
+        padding-bottom: 15px;
         text-align: center;
     }
 
     .navbar a {
         text-decoration: none;
         color: black;
+        font-family: Charter,monospace;
     }
 </style>
 <slot />

1a3b9889d957c7b9db578befc7b933aa20f53e5d
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2023-05-17 17:02:02 -0600

    Svelte
    Dev Svelte

---
+++ ReactiveWebsite/myapp/src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<nav class="navbar">
+    <a href="/">home</a>
+    <a href="/about">about</a>
+</nav>
+
+<style>
+    .navbar {
+        border: #ECD444;
+        border-bottom-style: solid;
+        border-bottom-width: 1px;
+        padding-bottom: 20px;
+        text-align: center;
+    }
+
+    .navbar a {
+        text-decoration: none;
+        color: black;
+    }
+</style>
+<slot />
---
+++ ReactiveWebsite/myapp/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+
+
+    <button class="start-btn">Start Visualizing</button>
+
+
+<style>
+    .start-btn {
+        width: 175px;
+        height: 50px;
+        transform: skew(-20deg);
+        background-color: #7B9CC2;
+        border: #ECD444;
+        border-bottom-style: solid;
+        border-right-style: solid;
+        font-family: Charter,monospace;
+        font-size: 1em;
+        font-weight: bold;
+        color: white;
+        letter-spacing: 1px;
+        transition: 0.3s;
+    }
+    .start-btn:hover {
+        background-color: #2541B2;
+    }
+
+</style>

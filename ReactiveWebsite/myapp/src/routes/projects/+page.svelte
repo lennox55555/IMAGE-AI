26f1f73dba84422beadc5e850a2fb7d6feb51a3b
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2023-07-19 13:05:56 -0600

    Delete ReactiveWebsite directory

--- ReactiveWebsite/myapp/src/routes/projects/+page.svelte
+++
@@ -1,8 +0,0 @@
-<h1>Projects</h1>
-<p> Projects demostrated over the past years </p>
-
-<ul>
-    <li><a href="/blog/one">one</a></li>
-    <li><a href="/blog/two">two</a></li>
-    <li><a href="/blog/three">three</a></li>
-</ul>
